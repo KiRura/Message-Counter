ad624f548106b81ca1c705f14b2a551a44b7e456
KiRura <kirura@noreply.com>
2023-11-07 00:42:47 +0900

    バグ修正

--- command/history.js
+++ command/history.js
@@ -24,7 +24,7 @@ export default {
     const guildsData = JSON.parse(fs.readFileSync('./data/guilds.json'))
     const guild = guildsData.find(guildData => guildData.id === interaction.guild.id)
     if (guild.history.length === 0) return await interaction.reply('データがひとつもありませんでした。')
-    const maxpages = (Math.floor(data.history.length / 10)) + 1
+    const maxpages = (Math.floor(guild.history.length / 10)) + 1
     if (maxpages < page) return await interaction.reply({ content: `${maxpages}ページまで指定できます`, ephemeral: true })
     const pageStart = 10 * (page - 1)
     const pageEnd = pageStart + 10
@@ -36,7 +36,7 @@ export default {
       embeds: [new EmbedBuilder()
         .setTitle('過去のデータ')
         .setDescription(`- ${history.join('\n- ')}${guild.history.length > 10 ? `\n\n残り: ${guild.history.length - 10}日分` : ''}`)
-        .setFooter(`${page} / ${maxpages} ページ`)
+        .setFooter({ text: `${page} / ${maxpages} ページ` })
         .setColor(data.mutaoColor)
       ]
     })
